05f9da40b55c18afc87a3795ee9f286cce47c7ae
her17138 <her17138@uvg.edu.gt>
2018-11-27 00:40:45 -0600

    movimiento suelo

--- Simulador/Assets/Scripts/Environment.cs
+++ Simulador/Assets/Scripts/Environment.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Environment : MonoBehaviour {
-    public GameObject edif2;
-    public GameObject edif3;
-    public GameObject edif4;
+    float[] piso1x;
     public bool moviendo = false;
     private Vector3 posGameObject;
     private float corrimiento;
-
+    int contadorPos = 0;
+    private int N; //cantidad de datos
     // Use this for initialization
     void Start () {
+
+        string[] lines = System.IO.File.ReadAllLines("Assets/Py/four_floors.dat");
+        N = lines.Length;
+        piso1x = new float[lines.Length]; 
+        int i = 0;
+        foreach (string line in lines)
+        {
+            // Use a tab to indent each line of the file.
+            string[] sep;
+            sep = line.Split();
+            piso1x[i] = float.Parse(sep[1]);
+            i++;
+        }
         posGameObject = this.gameObject.transform.position; 
         StartCoroutine("CorutinaUpdatePos");
 
@@ -23,12 +35,11 @@ public class Environment : MonoBehaviour {
 	}
     IEnumerator CorutinaUpdatePos()
     {
-        while (moviendo)
+        while (contadorPos < N)
         {
             yield return new WaitForSeconds(0.1f);
-            //promedios de posiciones
-            corrimiento = (edif2.gameObject.transform.position.x + edif3.gameObject.transform.position.x + edif4.gameObject.transform.position.x)/3.0f;
-            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x + corrimiento, this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+            this.gameObject.transform.position = new Vector3(this.gameObject.transform.position.x +(piso1x[contadorPos]/10f), this.gameObject.transform.position.y, this.gameObject.transform.position.z);
+            contadorPos++;
         }
     }
 }
